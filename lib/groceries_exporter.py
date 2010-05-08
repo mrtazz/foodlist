319067160e62090f682a686e6fd019a0de985947
Daniel Schauenberg <d@unwiredcouch.com>
2010-05-09 01:35:35 +0200

    minor fixes in GroceriesExporter

--- lib/groceries_exporter.py
+++ lib/groceries_exporter.py
@@ -4,6 +4,7 @@
 """
 
 import json
+import sys
 
 
 class GroceriesExporter:
@@ -14,11 +15,11 @@ class GroceriesExporter:
         try:
             aisles_json = json.loads(open(aisles_json_file).read())
         except IOError:
-            return -1
+            sys.exit(-1)
         self.aisles_raw = aisles_json["aisles"]
         self.aisles_master = {}
-        for aisle in aisles_raw:
-            aisles_master[aisle["name"]] = aisle["id"]
+        for aisle in self.aisles_raw:
+            self.aisles_master[aisle["name"]] = aisle["id"]
 
     def set_groceries(self, groceries):
         """ set groceries list """
@@ -29,12 +30,12 @@ class GroceriesExporter:
             returns it as string
         """
         data = {}
-        grocs = convert_groceries(groceries)
-        data["items"] = groceries
+        grocs = self.convert_groceries(groceries)
+        data["items"] = grocs
         data["name"] = name
         data["aisles"] = self.aisles_raw
 
-        return json.dumps(data)
+        return json.dumps(data).encode("utf-8")
 
     def convert_groceries(self, groceries_list):
         """ converts a groceries list into the
@@ -57,7 +58,7 @@ class GroceriesExporter:
                 new_item = {}
                 if aisle:
                     new_item["aisle"] = aisle
-                new_item["name"] = items
+                new_item["name"] = item
                 ret.append(new_item)
 
         return ret
