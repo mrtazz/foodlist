dcfee23784743ee6abaec08f70aa2cc07186e0c1
Daniel Schauenberg <d@unwiredcouch.com>
2010-05-23 02:22:30 +0200

    major refactoring
    - Custom exception classes
    - Abstraction for converter methods
    - Export only returns mustache context

--- foodlist/foodlist.py
+++ foodlist/foodlist.py
@@ -1,51 +1,39 @@
+# encoding: utf-8
 """
     foodlist
     module to produce Groceries.app importable output
 """
 
 import json
-import sys
 from urllib import quote
-
 
 class FoodList:
     """ Exporting a YAML groceries list as an import
         for Groceries.app
     """
-    def __init__(self, aisles_json_file):
-        try:
-            aisles_json = json.loads(open(aisles_json_file).read())
-        except IOError:
-            sys.exit(-1)
-        self.data = None
-        # the aisles as imported from the json dump
-        self.aisles_raw = aisles_json["aisles"]
-        # the aisles for matching the list items against
-        self.aisles_master = {}
-        # the list data which in different format
-        self.list_data = []
-        for aisle in self.aisles_raw:
-            self.aisles_master[aisle["name"]] = {"id" : aisle["id"],
-                                                 "image" : aisle["imageName"]}
-
-    def export_list(self, groceries, name = "Imported Groceries"):
-        """ export groceries to correct format and
-            returns it as string
+    def __init__(self):
+        """ init the available list formats
         """
-        if not self.data:
-            self.build_groceries_data(groceries, name)
-        return (self.list_data, quote(json.dumps(self.data)))
+        self.listformats = {
+                              "groceries.app" : self.convert_groceries_app
+                           }
 
-    def build_groceries_data(self, groceries, name = "Imported Groceries"):
-        """ build the complete groceries data set """
-        data = {}
-        grocs = self.convert_groceries(groceries)
-        data["items"] = grocs
-        data["name"] = name
-        data["aisles"] = self.aisles_raw
-        self.data = data
+    def export_list(self, groceries, name, format_, **kwargs):
+        """ export groceries list
 
-    def convert_groceries(self, groceries_list):
+            Parameters:
+                groceries -> the yaml parsed list
+                name -> the name of the output list
+                format_ -> the format for converting the list
+
+            Returns:
+                Context for requested format
+        """
+        return self.listformats.get(format_)(groceries, name, **kwargs)
+
+# converter methods for different formats
+
+    def convert_groceries_app(self, groceries_list, name, **kwargs):
         """ converts a groceries list into the
             format needed by groceries.app
 
@@ -54,18 +42,42 @@ class FoodList:
                      'aisle2': ['item3', 'item4']}
 
             Return:
-                array of groceries in correct format
+                context for mustache template
         """
-        ret = []
+
+
+        # open and parse json file
+        try:
+            json_data = kwargs["json_data"]
+        except KeyError:
+            raise MissingParameterError("Path to json dump of groceries.app\
+ base data not provided.")
+        try:
+            aisles_json = json.loads(open(json_data).read())
+        except IOError:
+            print json_data
+            raise BaseDataError("No groceries.app json dump available.")
+
+        # preparse groceries.app data for easy access later on
+        # the aisles as imported from the json dump
+
+        # the aisles for matching the list items against
+        aisles_master = {}
+        for aisle in aisles_json["aisles"]:
+            aisles_master[aisle["name"]] = {"id" : aisle["id"],
+                                            "image" : aisle["imageName"]}
+
+        list_data = []
+        groceries = []
         for aisles in groceries_list.keys():
             for item in groceries_list[aisles]:
                 try:
-                    aisle = self.aisles_master[aisles]
+                    aisle = aisles_master[aisles]
                 except KeyError:
                     aisle = None
                 # build data for groceries export
                 new_item = {}
-                if aisle:
+                if aisle is not None:
                     new_item["aisle"] = aisle["id"]
                 new_item["name"] = item
                 # build data for internal usage
@@ -74,8 +86,35 @@ class FoodList:
                     new_list_item["aisle"] = {"aislename":aisles,
                                               "aislecat": aisle["imageName"]}
                 new_list_item["name"] = item
-                self.list_data.append(new_list_item)
+                # append item to arrays
+                list_data.append(new_list_item)
+                groceries.append(new_item)
 
-                ret.append(new_item)
+        ctx = {}
+        ctx['groceries'] = quote(json.dumps({"items" : groceries, 'name' : name,
+                                             "aisles" : aisles_json['aisles']}))
+        ctx['listname'] = name
+        ctx['itemcount'] = len(groceries)
+        ctx["items"] = list_data
 
-        return ret
+        return ctx
+
+# Exception classes
+class BaseDataError(Exception):
+    """ exception to raise if data needed for constructing a specific format
+        is missing.
+    """
+    def __init__(self, arg):
+        self.arg = arg
+    def __str__(self):
+        return repr(self.arg)
+
+class MissingParameterError(Exception):
+    """ exception to raise if not all needed parameters were provided to
+        a converter method.
+    """
+    def __init__(self, arg):
+        self.arg = arg
+    def __str__(self):
+        return repr(self.arg)
+
